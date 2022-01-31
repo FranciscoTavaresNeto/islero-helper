0dbc2f8d6fd52934c3a74363945b7a405c43530b
FranciscoTavaresNeto <f.tavares928@gmail.com>
2022-01-31 00:47:12 -0300

    setup das rotas e esboço do login

--- islero-helper-angular/src/app/app-routing.module.ts
+++ islero-helper-angular/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalcComponent } from './calc/calc.component';
+import { CommitDeltaComponent } from './commit-delta/commit-delta.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { MaskAdminComponent } from './mask-admin/mask-admin.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LoginComponent
+  },
+  {
+    path: 'menu', component: MenuComponent
+  },
+  {
+    path: 'calc', component: CalcComponent
+  },
+  {
+    path: 'commit-delta', component: CommitDeltaComponent
+  },
+  {
+    path: 'dashboard', component: DashboardComponent
+  },
+  {
+    path: 'mask-admin', component: MaskAdminComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
